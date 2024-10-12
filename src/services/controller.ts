a17099761140738f97c8e10f5cbe1915f8a10245
CodeArmer <geanmotas@gmail.com>
2024-10-12 03:21:05 -0400

    Fixed the client directive

--- src/services/controller.ts
+++ src/services/controller.ts
@@ -1,13 +1,12 @@
 import { notes } from "./notes"
-export interface Note{
+export interface Note {
     id?: string
     title: string
     description: string
 }
 function generateId(): string {
     return `${Date.now()}-${Math.floor(Math.random() * 1000)}`;
-  }
-  
+}
 
 function createNote({title, description}: Note) {
     const newNote = {
@@ -16,7 +15,7 @@ function createNote({title, description}: Note) {
         description
     }
     const newNotes = [...notes, newNote]
-    
+
     return newNotes
 }
 function updateNote({id, description, title} : Note){
